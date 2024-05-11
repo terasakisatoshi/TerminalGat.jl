f4171432041541c15bda86e486386f38611f4c61
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2024-05-11 18:58:31 +0900

    implement src/TerminalGat.jl
    Add gat and gess functions to TerminalGat

--- src/TerminalGat.jl
+++ src/TerminalGat.jl
@@ -1,5 +1,25 @@
 module TerminalGat
 
-# Write your package code here.
+using gat_jll: gat_jll
+export gat, gess
 
+using IOCapture: IOCapture
+using TerminalPager: pager
+
+function gat(filename::AbstractString)
+    if splitext(filename)[end] == ".md"
+        run(`$(gat_jll.gat()) -M $(filename) --force-color`)
+    else
+        run(`$(gat_jll.gat()) $(filename) --force-color`)
+    end
+    nothing
 end
+
+function gess(filename::AbstractString)
+    c = IOCapture.capture() do
+        gat(filename)
+    end
+    c.output |> pager
+end
+
+end # module
