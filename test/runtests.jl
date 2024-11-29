226b9618ee4c00b255b32f393417f613636e5c42
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2024-11-29 23:40:19 +0900

    Introduce linting tools

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,12 @@
-using TerminalGat
 using Test
+using TerminalGat
+using Aqua
+using JET
 
-@testset "TerminalGat.jl" begin
-    # Write your tests here.
+@testset "Aqua" begin
+    Aqua.test_all(TerminalGat)
+end
+
+@testset "JET" begin
+    JET.report_package(TerminalGat, target_defined_modules=true)
 end
